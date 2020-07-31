6b9ea76d6ae4c8377d819ba523aca65009f793d6
EmmanuelSeyi <emmanuelaleke@gmail.com>
2020-07-31 14:33:08 +0100

    chane

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ app.set('view engine', 'ejs');
 //   res.render('index');
 // })
 
-app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
+
 // console.log('port');
 
 app.use(logger('dev'));
